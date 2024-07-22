5ab9c8cabe2015ff054094cdc69dff6fa4910f2a
Ren0503 <illuminati0440@gmail.com>
2024-07-22 11:12:26 +0700

    feat: add transform body

--- handler.go
+++ handler.go
@@ -2,29 +2,45 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/saas0503/factory-api/exception"
+	"github.com/saas0503/factory-api/pipe"
 	"net/http"
 )
 
-type Req struct {
-	*http.Request
-}
-
-type Res struct {
-	http.ResponseWriter
-}
-
 type ResponseOptions struct {
-	data    interface{}
-	total   *int
-	message *string
+	Data    interface{}
+	Total   *int
+	Message *string
+}
+
+func TransformBody[P any](w http.ResponseWriter, r *http.Request) *P {
+	var payload P
+
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		exception.ThrowInvalidRequest(w, err)
+		return nil
+	}
+
+	errors := pipe.ValidateStruct(&payload)
+	if errors != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(errors)
+		if err != nil {
+			return nil
+		}
+		return nil
+	}
+
+	return &payload
 }
 
 func JSON(w http.ResponseWriter, res ResponseOptions) {
 	response := map[string]interface{}{
 		"status":  "success",
-		"data":    res.data,
-		"message": *res.message,
-		"total":   *res.total,
+		"data":    res.Data,
+		"message": *res.Message,
+		"total":   *res.Total,
 	}
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(response)
