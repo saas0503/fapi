65e5a76aa9d602fded10681dfde1c37970bcb437
Ren0503 <illuminati0440@gmail.com>
2024-07-25 18:21:15 +0700

    feat: add module

--- module.go
+++ module.go
@@ -1,6 +1,38 @@
 package api
 
 type Module struct {
+	app *App
+	mux Mux
+}
+
+type ModuleOptions struct {
 	Imports    []*Module
 	Controller []*Controller
+}
+
+func NewModule(opt ModuleOptions) *Module {
+	mux := make(Mux)
+	for _, m := range opt.Imports {
+		for k, v := range m.mux {
+			mux[k] = v
+		}
+		m = nil
+	}
+
+	for _, ctrl := range opt.Controller {
+		for k, v := range ctrl.mux {
+			mux[k] = v
+		}
+		ctrl = nil
+	}
+
+	return &Module{
+		mux: mux,
+	}
+}
+
+func CreateApp(module *Module) *App {
+	return &App{
+		mux: module.mux,
+	}
 }
